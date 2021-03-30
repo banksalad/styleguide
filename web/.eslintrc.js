683b6f9f02da5fcba2860a1820807c2c0ddba14e
YoungChang Lee <apricotsoul@gmail.com>
2021-03-30 12:20:12 +0900

    Address comment

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -38,7 +38,6 @@ module.exports = {
     "@typescript-eslint/no-shadow": "warn",
     "@typescript-eslint/ban-types": "warn",
     "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-shadow": "warn",
     /* 명시적인 return type 작성*/
     "@typescript-eslint/explicit-module-boundary-types": "off",
 
